3c5702e7002279ad7b708372d57eb64913f24fc5
Max Steffen <max.steffen@mail.com>
2023-12-14 11:16:07 +0100

    add navigator 1.0 example

--- lib/clean_architecture/presentation/screens/home_screen.dart
+++ lib/clean_architecture/presentation/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_schulung/clean_architecture/presentation/pages/advice_page.dart';
+import 'package:flutter_schulung/clean_architecture/presentation/screens/settings_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -9,8 +10,29 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Clean Architecture'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) {
+                    return const SettingsScreen();
+                  },
+                ),
+              );
+            },
+            icon: const Icon(Icons.settings),
+          ),
+        ],
       ),
       body: const AdvicePageProvider(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // TODO open add Advice Screen
+        },
+        child: const Icon(Icons.plus_one),
+      ),
     );
   }
 }
